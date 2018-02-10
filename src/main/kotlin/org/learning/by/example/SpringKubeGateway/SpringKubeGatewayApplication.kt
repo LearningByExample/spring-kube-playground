bf511ac9f9b0951d2c1225d7d0745b0afbf50b2b
Juan Medina <jamedina@gmail.com>
2018-02-10 13:32:17 +0000

    first commit

---
+++ src/main/kotlin/org/learning/by/example/SpringKubeGateway/SpringKubeGatewayApplication.kt
@@ -0,0 +1,11 @@
+package org.learning.by.example.SpringKubeGateway
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringKubeGatewayApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringKubeGatewayApplication>(*args)
+}
