69be9ec25815f3aecb47830d17f01e3f06785cbc
Juan Medina <jamedina@gmail.com>
2018-03-08 08:15:27 +0000

    adding eureka server

---
+++ EurekaServer/src/main/kotlin/org/learning/by/example/SpringKubeGateway/EurekaServer/EurekaServerApplication.kt
@@ -0,0 +1,13 @@
+package org.learning.by.example.SpringKubeGateway.EurekaServer
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer
+
+@SpringBootApplication
+@EnableEurekaServer
+class EurekaServerApplication
+
+fun main(args: Array<String>) {
+    runApplication<EurekaServerApplication>(*args)
+}
---
+++ EurekaServer/src/test/kotlin/org/learning/by/example/SpringKubeGateway/EurekaServer/EurekaServerApplicationTests.kt
@@ -0,0 +1,16 @@
+package org.learning.by.example.SpringKubeGateway.EurekaServer
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
