5169a16ea0c813d824baf1993fbd9435d74f8dbc
Juan Medina <jamedina@gmail.com>
2018-03-14 07:39:10 +0000

    [#4] adding initial code to the gateway that will use the load balance / eureka to route to the microservice

--- SpringKubeGateway/src/main/kotlin/org/learning/by/example/SpringKubeGateway/SpringKubeGatewayApplication.kt
+++ SpringKubeGateway/src/main/kotlin/org/learning/by/example/SpringKubeGateway/SpringKubeGatewayApplication.kt
@@ -2,9 +2,22 @@ package org.learning.by.example.SpringKubeGateway
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder
+import org.springframework.cloud.gateway.route.builder.routes
+import org.springframework.context.annotation.Bean
 
 @SpringBootApplication
-class SpringKubeGatewayApplication
+class SpringKubeGatewayApplication {
+
+  @Bean
+  fun routes(routes: RouteLocatorBuilder) = routes.routes {
+    route {
+      path("/hello")
+      uri("lb://simplemicroservice/hello")
+    }
+  }
+
+}
 
 fun main(args: Array<String>) {
   runApplication<SpringKubeGatewayApplication>(*args)
