2ff06c30a2493db1308070bac795d1793fc2df0f
Juan Medina <jamedina@gmail.com>
2018-03-15 08:11:15 +0000

    [#4] adding authomatic routes from eureka registered services

--- SpringKubeGateway/src/main/kotlin/org/learning/by/example/SpringKubeGateway/SpringKubeGatewayApplication.kt
+++ SpringKubeGateway/src/main/kotlin/org/learning/by/example/SpringKubeGateway/SpringKubeGatewayApplication.kt
@@ -2,12 +2,21 @@ package org.learning.by.example.SpringKubeGateway
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.client.discovery.DiscoveryClient
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder
 import org.springframework.cloud.gateway.route.builder.routes
 import org.springframework.context.annotation.Bean
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator
+
 
 @SpringBootApplication
+@EnableDiscoveryClient
 class SpringKubeGatewayApplication {
+
+  @Bean
+  fun discoveryClientRouteLocator(discoveryClient: DiscoveryClient) =
+      DiscoveryClientRouteDefinitionLocator(discoveryClient)
 
   @Bean
   fun routes(routes: RouteLocatorBuilder) = routes.routes {
