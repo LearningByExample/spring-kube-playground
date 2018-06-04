c22abd50687657ab8210c2baacec4284a82ca22e
Juan Medina <jamedina@gmail.com>
2018-06-04 08:38:19 +0100

    update project

--- spring-kube-gateway/src/main/kotlin/org/learning/by/example/spring/kube/playground/spring/kube/gateway/GatewayApplication.kt
+++ spring-kube-gateway/src/main/kotlin/org/learning/by/example/spring/kube/playground/spring/kube/gateway/GatewayApplication.kt
@@ -4,6 +4,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.cloud.client.discovery.DiscoveryClient
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties
 import org.springframework.context.annotation.Bean
 
 
@@ -12,7 +13,7 @@ import org.springframework.context.annotation.Bean
 class GatewayApplication {
 
   @Bean
-  fun discoveryClientRouteLocator(discoveryClient: DiscoveryClient) =
-      DiscoveryClientRouteDefinitionLocator(discoveryClient)
+  fun discoveryClientRouteLocator(discoveryClient: DiscoveryClient, discoveryLocatorProperties: DiscoveryLocatorProperties) =
+      DiscoveryClientRouteDefinitionLocator(discoveryClient, discoveryLocatorProperties)
 
 }
